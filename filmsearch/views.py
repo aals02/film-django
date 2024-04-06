79306e100bc38f94071d5f6c46ad1c9d8e616910
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-06 23:28:48 +0100

    Update views.py
    new

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 from .models import Film
 
 def filmlist(request):
@@ -9,3 +10,16 @@ def filmlist(request):
 def filmlist23(request):
     items = Film.objects.filter(name='abc')
     return render(request, 'listfilm.html', {'items': items})
+
+#  user profile view
+from .models import User
+def userProfile(request):
+    items = User.objects.all()
+    return render(request, 'listfilm.html', {'items': items})
+# validation for objects like name
+
+#  user friend list view
+# from .models import User
+# def friendList(request):
+#     items = Friends.objects.all()
+#     return render(request, 'listfilm.html', {'items': items})
