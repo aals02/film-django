e3be4ee5a640827bacc15ee83823def2cb64a1c9
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-06 23:57:07 +0100

    Update models.py

--- filmsearch/models.py
+++ filmsearch/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Film(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
 # user profile database
 class User(models.Model):
     name = models.CharField(max_length=100)
