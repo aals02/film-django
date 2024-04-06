10b6522cca67c0c781053c90edc27c25805cd154
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-06 23:56:25 +0100

    Update urls.py
    remove fim

--- film/urls.py
+++ film/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from filmsearch.views import filmlist, filmlist23, userProfile
+from filmsearch.views import userProfile
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("items/", filmlist, name="filmlist"),
-    path("itemsfiltered/", filmlist23, name="filmlist23"),
     path("profile/", userProfile, name="userProfile")
 ]
