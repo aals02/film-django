d71d23eef55a9459385efaa11f908f5f3474e34f
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 15:06:08 +0100

    Update forms.py

--- filmsearch/forms.py
+++ filmsearch/forms.py
@@ -1,1 +1,15 @@
+from django import forms
+from .models import User
 
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'age', 'username', 'password', 'repeat_password', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        repeat_password = cleaned_data.get('repeat_password')
+
+        if password != repeat_password:
+            raise forms.ValidationError("The passwords do not match.")
