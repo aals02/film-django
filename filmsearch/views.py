e0a6b51424b88ead18e1fe141a6c21cad50837ce
mo2802 <sgpmonic@liverpool.ac.uk>
2024-04-07 20:12:47 +0100

    Connected to TMDB API and performed requests

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
+from .APIConnection import connect
+from .models import Films
 
 # Create your views here.
 
@@ -14,14 +16,39 @@ def userProfile(request):
     else:
         form = UserForm()
     return render(request, 'profileUser.html', {'form': form})
-    
+
     # items = User.objects.all()
     # return render(request, 'profileUser.html', {'items': items})
 # add query to add user data from form
-# add query to get only user details of user thats logged in
+# add query to get only user details of user that logged in
 
 #  user friend list view
 # from .models import User
 # def friendList(request):
 #     items = Friends.objects.all()
 #     return render(request, 'listfilm.html', {'items': items})
+
+
+def movie_List(request):
+
+    api_films = connect()  # This should return a list of dictionaries
+
+    # You might want to clear the Films table before repopulating it
+    # Be cautious with this as it will delete all existing entries in the Films table
+    # Films.objects.all().delete()
+
+    # Create Films objects only if they don't already exist
+    for film_data in api_films:
+        Films.objects.get_or_create(
+            name=film_data['original_title'],
+            defaults={'description': film_data['overview'],
+                    'poster_image': f"https://media.themoviedb.org/t/p/w600_and_h900_bestv2{film_data['poster_path']}"}
+        )
+
+    films = Films.objects.all()
+
+    print(api_films)
+    return render(request, 'movieRecs.html', {'films': films})
+
+
+
