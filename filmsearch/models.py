d50112d7ccf2e6eac24adfe8ddc1563a4cd848d4
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 14:51:28 +0100

    Update models.py

--- filmsearch/models.py
+++ filmsearch/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
@@ -6,9 +7,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100, blank = False, null = False)
     age = models.IntegerField(blank = False, null = False)
-    username = models.CharField(min_length=6, max_length=100, blank = False, null = False)
-    password = models.CharField(min_length=8, max_length=100, blank = False, null = False)
-    repeat_password = models.CharField(min_length=8, max_length=100, blank = False, null = False)
+    username = models.CharField(validators=[MinLengthValidator(6)], max_length=100, blank = False, null = False)
+    password = models.CharField(validators=[MinLengthValidator(8)], max_length=100, blank = False, null = False)
+    repeat_password = models.CharField(validators=[MinLengthValidator(8)], max_length=100, blank = False, null = False)
     email = models.EmailField(blank = False, null = False)
     
     def __str__(self):
