7c3c63f85c73cfb62e1272720c37dc5ff919d599
samiya-03 <128643423+samiya-03@users.noreply.github.com>
2024-04-07 15:25:47 +0100

    Update forms.py

--- filmsearch/forms.py
+++ filmsearch/forms.py
@@ -1,3 +1,6 @@
+Here's the code with proper indentations:
+
+```python
 from django import forms
 from .models import User
 from formValidationApp.models import *
@@ -13,7 +16,6 @@ class UserForm(forms.ModelForm):
         password = self.cleaned_data.get('password')
         repeat_password = self.cleaned_data.get('repeat_password')
         username = self.cleaned_data.get('username')
-        
 
         if password != repeat_password:
             raise forms.ValidationError("The passwords do not match.")
@@ -21,11 +23,11 @@ class UserForm(forms.ModelForm):
         if len(password) < 6:
             self._errors['password'] = self.error_class([
                 'Minimum 8 characters required'])
-            
+
         if len(password) > 100:
-           self._errors['password'] = self.error_class([
+            self._errors['password'] = self.error_class([
                 'Maximum 100 characters allowed'])        
-            
+
         if len(username) < 6:
             self._errors['username'] = self.error_class([
                 'Minimum 6 characters required'])
@@ -33,11 +35,8 @@ class UserForm(forms.ModelForm):
         if len(username) > 100:
             self._errors['username'] = self.error_class([
                 'Maximum 100 characters required'])
-            
-            
 
-         return self.cleaned_data
-        
-            
+        return self.cleaned_data
+```
 
-
+I've fixed the indentations to follow the Python standard. Each block of code within a function should be indented consistently.
