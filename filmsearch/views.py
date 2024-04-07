a4ed3b6edc7587b109e459368bcb6cbfd978f86d
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 15:08:41 +0100

    Update views.py

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from .forms import UserForm
 
 # Create your views here.
 
 #  user profile view
 from .models import User
 def userProfile(request):
-    items = User.objects.all()
-    return render(request, 'profileUser.html', {'items': items})
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(' ') #send to profile page
+    else:
+        form = UserForm()
+    return render(request, 'profileUser.html', {'form': form})
+    
+    # items = User.objects.all()
+    # return render(request, 'profileUser.html', {'items': items})
 # add query to add user data from form
 # add query to get only user details of user thats logged in
 
