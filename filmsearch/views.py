37e858bf811a5f3bbee93b82bea6eb7a142751d7
samiya-03 <128643423+samiya-03@users.noreply.github.com>
2024-04-07 23:12:11 +0100

    Update views.py

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -6,19 +6,19 @@ from .forms import UserForm
 #  user profile view
 from .models import User
 def userProfile(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('') #send to profile page
-    else:
-        form = UserForm()
-    return render(request, 'profileUser.html', {'form': form})
+    # if request.method == 'POST':
+    #     form = UserForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('') #send to profile page
+    # else:
+    #     form = UserForm()
+    # return render(request, 'profileUser.html', {'form': form})
 
 
     
-    # items = User.objects.all()
-    # return render(request, 'profileUser.html', {'items': items})
+     items = User.objects.all()
+     return render(request, 'profileUser.html', {'items': items})
 # add query to add user data from form
 # add query to get only user details of user thats logged in
 
