4ab3e17029cf64a9ef1421f1026fa3e1bd1f7152
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 12:22:46 +0100

    Update views.py

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -8,7 +8,8 @@ def userProfile(request):
     items = User.objects.all()
     return render(request, 'listfilm.html', {'items': items})
 # validation for objects like name
-#add query to get only user details of user thats logged in
+# add query to add user data from form
+# add query to get only user details of user thats logged in
 
 #  user friend list view
 # from .models import User
