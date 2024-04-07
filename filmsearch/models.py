7b4168006882acd6a2918b16723b0543ec16d32a
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 14:31:40 +0100

    Update models.py

--- filmsearch/models.py
+++ filmsearch/models.py
@@ -4,20 +4,20 @@ from django.db import models
 
 # user profile database
 class User(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    repeat_password = models.CharField(max_length=100)
-    email = models.EmailField()
+    name = models.CharField(max_length=100, blank = False, null = False)
+    age = models.IntegerField(blank = False, null = False)
+    username = models.CharField(max_length=100, blank = False, null = False)
+    password = models.CharField(max_length=100, blank = False, null = False)
+    repeat_password = models.CharField(max_length=100, blank = False, null = False)
+    email = models.EmailField(blank = False, null = False)
     
     def __str__(self):
         return self.name
     
 # friend list
 # class Friends(models.Model):
-#     friend_name = models.CharFields(max_length=100)
-#     friend_username = models.CharFields(max_length=100)
+#     friend_name = models.CharFields(max_length=100, blank = False, null = False)
+#     friend_username = models.CharFields(max_length=100, blank = False, null = False)
     
 #     def __str__(self):
 #         return self.friend_name
