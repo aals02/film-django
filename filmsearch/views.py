c731121791657db8e760f339183688e2bbddd851
samiya-03 <128643423+samiya-03@users.noreply.github.com>
2024-04-07 23:34:33 +0100

    Update views.py

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -22,8 +22,10 @@ def userProfile(request):
 # add query to add user data from form
 # add query to get only user details of user thats logged in
 
-#  user friend list view
-# from .models import User
-# def friendList(request):
-#     items = Friends.objects.all()
-#     return render(request, 'listfilm.html', {'items': items})
+ user friend list view
+from .models import User
+def friendList(request):
+    items = Friends.objects.all()
+    return render(request, 'listfilm.html', {'items': items})
+
+
