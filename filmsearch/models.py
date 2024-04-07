bc7d88f0023acd5479ab6da766a27db05a5d0351
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-07 14:33:04 +0100

    Update models.py

--- filmsearch/models.py
+++ filmsearch/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100, blank = False, null = False)
     age = models.IntegerField(blank = False, null = False)
-    username = models.CharField(max_length=100, blank = False, null = False)
-    password = models.CharField(max_length=100, blank = False, null = False)
-    repeat_password = models.CharField(max_length=100, blank = False, null = False)
+    username = models.CharField(min_length=6, max_length=100, blank = False, null = False)
+    password = models.CharField(min_length=8, max_length=100, blank = False, null = False)
+    repeat_password = models.CharField(min_length=8, max_length=100, blank = False, null = False)
     email = models.EmailField(blank = False, null = False)
     
     def __str__(self):
