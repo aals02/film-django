fd4b30ae88772b4635cf2da818ff407bae45b738
aaliyah <158219401+aals02@users.noreply.github.com>
2024-04-08 00:18:32 +0100

    Update views.py

--- filmsearch/views.py
+++ filmsearch/views.py
@@ -13,16 +13,25 @@ def userProfile(request):
     #     form = UserForm()
     # return render(request, 'profileUser.html', {'form': form})
 
-
-    
      items = User.objects.all()
      return render(request, 'profileUser.html', {'items': items})
-# add query to add user data from form
-# add query to get only user details of user thats logged in
 
  # user friend list view
 def friendList(request):
     items = Friends.objects.all()
     return render(request, 'listfilm.html', {'items': items})
+
+
+# add query to add user data from form
+# add query to display/get user details of user thats logged in for profile
+# validate password
+# api queries
+# query for adding movies to databse user swipes right on and left on
+# query to fetch movies user said yes to
+# query to add friends
+# query to fetch/display users friends 
+# query for log in if username and password match user in database
+# forgot password part - send email to reset
+# allowing user to edit profile - query to alter/update database
 
 
