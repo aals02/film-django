02c615da4e11d5cd293227571afbf7c86744e87f
Rafiah-n <54577192+Rafiah-n@users.noreply.github.com>
2024-04-16 16:43:49 +0100

    removed xiaowei's extra file after embedding his code within other files

--- filmsearch/apps.py
+++ filmsearch/apps.py
@@ -5,4 +5,7 @@ class FilmsearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "filmsearch"
 
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
 
