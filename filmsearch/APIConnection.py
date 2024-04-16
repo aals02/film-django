5ed17f373036d131f2025c7c84bd7f014ed681e9
Rafiah-n <54577192+Rafiah-n@users.noreply.github.com>
2024-04-16 19:55:41 +0100

    created api request for genres and extra information for movies

--- filmsearch/APIConnection.py
+++ filmsearch/APIConnection.py
@@ -1,12 +1,13 @@
 import requests
 
-def connect(firstpage, lastpage):
+def retrieve_movies(firstpage, lastpage):
     print("connected")
     output = list()
 
+    api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NWI4ZDFkNTVhZmRjYWUwOGVjMDQ4ZWVlNjRmZDU3ZCIsInN1YiI6IjY2MGViODYzZTE4ZTNmMDE0YWEzMmYyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oNwVMAnLjuU9ptyWKIVqqPJiUor6KFn61wU09Dxy1CI"
+
     for page in range(firstpage, lastpage + 1):
         url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page={page}&sort_by=popularity.desc"
-        api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NWI4ZDFkNTVhZmRjYWUwOGVjMDQ4ZWVlNjRmZDU3ZCIsInN1YiI6IjY2MGViODYzZTE4ZTNmMDE0YWEzMmYyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oNwVMAnLjuU9ptyWKIVqqPJiUor6KFn61wU09Dxy1CI"
         headers = {
             "accept": "application/json",
             "Authorization": f"Bearer {api_key}"
@@ -18,3 +19,20 @@ def connect(firstpage, lastpage):
     
     return output
 
+def retrieve_genres():
+    api_key = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NWI4ZDFkNTVhZmRjYWUwOGVjMDQ4ZWVlNjRmZDU3ZCIsInN1YiI6IjY2MGViODYzZTE4ZTNmMDE0YWEzMmYyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oNwVMAnLjuU9ptyWKIVqqPJiUor6KFn61wU09Dxy1CI"
+    output = list()
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # This will raise an exception for HTTP errors
+        data = response.json()
+        return data.get('genres', [])  # Returns an empty list if 'genres' key is missing
+    except requests.RequestException as e:
+        print(f"Failed to retrieve genres: {e}")
+        return []
--- filmsearch/management/commands/api_call.py
+++ filmsearch/management/commands/api_call.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 
-from filmsearch.APIConnection import connect
-from filmsearch.models import Films
+from filmsearch.APIConnection import retrieve_movies, retrieve_genres
+from filmsearch.models import Films, Genre
 
 
 class Command(BaseCommand):
@@ -13,11 +13,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print(options)
-        api_films = connect(options["firstpage"][0], options["lastpage"][0])  # This should return a list of dictionaries
+        api_films = retrieve_movies(options["firstpage"][0], options["lastpage"][0])  # This should return a list of dictionaries
 
         # You might want to clear the Films table before repopulating it
         # Be cautious with this as it will delete all existing entries in the Films table
         # Films.objects.all().delete()
+
+        genres = retrieve_genres()
+        for genre_data in genres:
+            Genre.objects.get_or_create(
+                api_genre_id = genre_data['id'],
+                defaults = {
+                    'genre_name': genre_data['name']}
+            )
 
         # Create Films objects only if they don't already exist
         for film_data in api_films:
@@ -25,10 +33,14 @@ class Command(BaseCommand):
             print("poster_path:", film_data['poster_path'])  # This will print the poster_path from the API
             print("Poster URL:", poster_url)  # This will print the full URL
 
-            film, created = Films.objects.get_or_create(
+            Films.objects.get_or_create(
                 name=film_data['original_title'],
                 defaults={
                     'description': film_data['overview'],
-                    'poster_image': poster_url
+                    'poster_image': poster_url,
+                    'lang': film_data['original_language'],
+                    'rating': film_data['vote_average'],
+                    'release_date': film_data['release_date'],
+                    'genres': film_data['genre_ids']
                 }
             )
