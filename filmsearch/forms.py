50a07c52299e75b22eccfac26e0741f0f30cdd18
samiya-03 <128643423+samiya-03@users.noreply.github.com>
2024-04-16 02:31:55 +0100

    Update forms.py

--- filmsearch/forms.py
+++ filmsearch/forms.py
@@ -1,23 +1,10 @@
 from django import forms
 from .models import User
-from logging import PlaceHolder
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 
 User = get_user_model()
-
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password')
 
 class UserForm(forms.ModelForm):
     # name = forms.CharField(widget=forms.TextInput(attrs={
