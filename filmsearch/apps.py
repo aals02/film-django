cd87987ac6ae332bb5cf7c8076645a552c81da5c
samiya-03 <128643423+samiya-03@users.noreply.github.com>
2024-04-16 02:05:11 +0100

    Update apps.py

--- filmsearch/apps.py
+++ filmsearch/apps.py
@@ -4,6 +4,4 @@ from django.apps import AppConfig
 class FilmsearchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "filmsearch"
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+
