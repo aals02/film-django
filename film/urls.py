7e954c72045ac5b0e495b64124b1b8407ac595c9
RiverKuiper <95480551+RiverKuiper@users.noreply.github.com>
2024-04-17 13:12:32 +0100

    Update urls.py

--- film/urls.py
+++ film/urls.py
@@ -29,5 +29,9 @@ urlpatterns = [
     path("signup/", signup, name="signup"),
     path("login/", login_view, name="login"),
     path("preferences/", movie_preference, name='movie-preferences'),
-    path('', redirect_to_homepage, name='home')
+    path('', redirect_to_homepage, name='home'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
